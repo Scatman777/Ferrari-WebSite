d26d3cace91c62d4f8623b5a46dc127f70e0de42
hostr777 <divakovbogdan11@gmail.com>
2023-09-05 20:37:10 +0300

    Update

--- js/Ferrari.js
+++ js/Ferrari.js
@@ -2,3 +2,18 @@ function buttonMap() {
 	document.getElementById("Map").style.backgroundImage = "url('/img/Mapsicle Map Black.svg')"
 	document.getElementById("Display").style.display = "block"
 }
+
+var swiper = new Swiper(".swiper", {
+	spaceBetween: 20,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	watchOverflow: true,
+	slidesPerView: 'auto',
+	slidesPerGroup: 1,
+	slidesPerColumn: 1,
+	dots: false,
+	mousewheel: true,
+	keyboard: true,
+  });
